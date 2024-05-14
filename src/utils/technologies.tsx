7893f92689b899480abe9fc11f7bca3dbfb9ada8
DaivyMorales <daivymorales7705@gmail.com>
2024-05-14 17:57:35 -0500

    feat: svg tech category

--- src/utils/technologies.tsx
+++ src/utils/technologies.tsx
@@ -3,6 +3,11 @@ export interface Technologies {
   icon: string;
 }
 
+export interface Tech {
+  name: string;
+  url: string;
+}
+
 export const allTechnologies = [
   {
     icon: "LuHammer",
@@ -19,7 +24,81 @@ export const allTechnologies = [
 ];
 
 export const toolsTech = [
-    {
-        url: ""
-    }
-]
+  {
+    name: "Figma",
+    url: "/figma.svg",
+  },
+  {
+    name: "Visual Studio Code",
+    url: "/vscode.svg",
+  },
+  {
+    name: "Next.js",
+    url: "/nextjs.svg",
+  },
+  {
+    name: "ReduxJS",
+    url: "/redux.svg",
+  },
+  {
+    name: "Git",
+    url: "/git.svg",
+  },
+  {
+    name: "Vercel",
+    url: "/vercel.svg",
+  },
+];
+
+export const frontTech = [
+  {
+    name: "React",
+    url: "/react.svg",
+  },
+  {
+    name: "HTML5",
+    url: "/html5.svg",
+  },
+  {
+    name: "CSS",
+    url: "/css.svg",
+  },
+  {
+    name: "Tailwind CSS",
+    url: "/tailwindcss.svg",
+  },
+  {
+    name: "Npm",
+    url: "/npm.svg",
+  },
+  {
+    name: "JavaScript",
+    url: "/javascript.svg",
+  },
+];
+export const backTech = [
+  {
+    name: "NodeJS",
+    url: "/nodejs.svg",
+  },
+  {
+    name: "Prisma",
+    url: "/prisma-2.svg",
+  },
+  {
+    name: "Supabase",
+    url: "/supabase.svg",
+  },
+  {
+    name: "MongoDB",
+    url: "/mongodb.svg",
+  },
+  {
+    name: "Trpc",
+    url: "/trpc.svg",
+  },
+  {
+    name: "TypeScript",
+    url: "/typescript.svg",
+  },
+];
